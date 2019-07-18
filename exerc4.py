10abd7d10b166f263e79e937d56bf6c5e53afa38
mmtoth <milena.mt@gmail.com>
2019-07-18 14:32:35 -0300

    Resolução do exercício 4 - bhaskara

--- exerc4.py
+++ exerc4.py
@@ -2,14 +2,17 @@ import math
 
 print('Função de segundo grau')
 
-A = int(input('Digite o valor de A: '))
-B = int(input('Digite o valor de B: '))
-C = int(input('Digite o valor de C: '))
+A = float(input('Digite o valor de A: '))
+B = float(input('Digite o valor de B: '))
+C = float(input('Digite o valor de C: '))
 
 delta = (B ** 2) - (4 * A * C)
-raiz_delta = math.sqrt(delta)
-bhaskara1 = (-B + raiz_delta) / (2 * A)
-bhaskara2 = (-B - raiz_delta) / (2 * A)
 
-print('Resultado X:', bhaskara1)
-print('Resultado Y:', bhaskara2)
+if delta>=0:
+    raiz_delta = math.sqrt(delta)
+    x1 = (-B + raiz_delta) / (2 * A)
+    x2 = (-B - raiz_delta) / (2 * A)
+    print('Resultado X1:', x1)
+    print('Resultado X2:', x2)
+else:
+    print('Delta negativo. Não foi possível')
